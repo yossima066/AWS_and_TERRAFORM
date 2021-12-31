ef0346130080fe1ea34e6c29d9329f80a076526e
yossima066 <yossima066@gmail.com>
2021-12-31 22:18:05 +0200

    blink eks.tf

--- midProj/Eks.tf
+++ midProj/Eks.tf
@@ -1,5 +1,5 @@
-module "eks" {
-  source          = "D:\\terraform\\mid-project\\eks-terraform"
-  vpc_id          = module.main_vpc.aws_vpc_id
-  private_subnets = [module.private_subnet_1.aws_subnet_id, module.private_subnet_2.aws_subnet_id]
-}
+# module "eks" {
+#   source          = "D:\\terraform\\mid-project\\eks-terraform"
+#   vpc_id          = module.main_vpc.aws_vpc_id
+#   private_subnets = [module.private_subnet_1.aws_subnet_id, module.private_subnet_2.aws_subnet_id]
+# }
