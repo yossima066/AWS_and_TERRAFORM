b392681ffbd282e3cf2df5d636dd0f72eb07a31c
yossima066 <yossima066@gmail.com>
2022-01-02 09:08:13 +0200

    add /target-groups/variables.tf target-groups/main.tf

--- modules/target-groups/main.tf
+++ modules/target-groups/main.tf
@@ -4,12 +4,9 @@ resource "aws_lb_target_group" "tg" {
   protocol = var.protocol
   vpc_id   = var.vpc_id
   health_check {
-    matcher = "200"
-    path    = "/index.html"
+    matcher = var.health_check_matcher
+    path    = var.health_check_path
+    port    = var.health_check_port
   }
   target_type = var.target_type
-  stickiness {
-    cookie_duration = 60
-    type            = "lb_cookie"
-  }
 }
--- modules/target-groups/variables.tf
+++ modules/target-groups/variables.tf
@@ -19,3 +19,18 @@ variable "target_type" {
   type    = string
   default = "instance"
 }
+
+variable "health_check_path" {
+  type    = string
+  default = "/"
+}
+
+variable "health_check_port" {
+  type    = number
+  default = 8500
+}
+
+variable "health_check_matcher" {
+  type    = number
+  default = 301
+}
